571583070f46674a69333ab1682d3de6b91dce7d
dang\tranvandang <dangtvph42342@fpt.edu.vn>
2023-11-20 09:06:44 +0700

    tiếp

--- Books/src/books/view/ViewHoaDon.java
+++ Books/src/books/view/ViewHoaDon.java
@@ -176,7 +176,7 @@ public class ViewHoaDon extends javax.swing.JPanel {
         txtSoHoaDon1 = new javax.swing.JLabel();
         jScrollPane6 = new javax.swing.JScrollPane();
         tblHoaDon2 = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
+        BtTimKiem = new javax.swing.JButton();
         jTabbedPane7 = new javax.swing.JTabbedPane();
         jPanel6 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -272,9 +272,14 @@ public class ViewHoaDon extends javax.swing.JPanel {
         ));
         jScrollPane6.setViewportView(tblHoaDon2);
 
-        jButton1.setBackground(new java.awt.Color(0, 255, 204));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/books/icons/Search.png"))); // NOI18N
-        jButton1.setText("tìm");
+        BtTimKiem.setBackground(new java.awt.Color(0, 255, 204));
+        BtTimKiem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/books/icons/Search.png"))); // NOI18N
+        BtTimKiem.setText("tìm");
+        BtTimKiem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtTimKiemActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
@@ -300,7 +305,7 @@ public class ViewHoaDon extends javax.swing.JPanel {
                         .addGap(18, 18, 18)
                         .addComponent(jTextField9, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton1)))
+                        .addComponent(BtTimKiem)))
                 .addContainerGap(717, Short.MAX_VALUE))
             .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel5Layout.createSequentialGroup()
@@ -315,7 +320,7 @@ public class ViewHoaDon extends javax.swing.JPanel {
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel13)
                     .addComponent(jTextField9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                    .addComponent(BtTimKiem))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel14)
@@ -542,9 +547,14 @@ public class ViewHoaDon extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void BtTimKiemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtTimKiemActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_BtTimKiemActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton BtTimKiem;
     private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
     private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem2;
     private javax.swing.JComboBox<String> jComboBox2;
